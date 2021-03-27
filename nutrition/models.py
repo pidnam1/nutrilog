e60c5287fa0b4eaf55faf2c013aa8f27f9774760
pidnam1 <pidnam1@gmail.com>
2021-03-27 18:08:40 -0400

    new stuff

--- nutrition/models.py
+++ nutrition/models.py
@@ -3,3 +3,11 @@ from django.db import models
 # Create your models here.
 class List(models.Model):
     list_Img = models.ImageField(upload_to='images/')
+
+class Food(models.Model):
+    name = models.CharField(max_length=50)
+    calories = models.DecimalField(max_digits=6, decimal_places=2)
+    carbs = models.DecimalField(max_digits=6, decimal_places=2)
+    protein = models.DecimalField(max_digits=6, decimal_places=2)
+    fat = models.DecimalField(max_digits=6, decimal_places=2)
+    sugar = models.DecimalField(max_digits=6, decimal_places=2)
--- nutrition/views.py
+++ nutrition/views.py
@@ -4,6 +4,7 @@ import requests
 import json
 from google.cloud import vision
 from .forms import *
+from .models import Food
 
 def index(request):
     return render(request, 'nutrition/home.html')
@@ -16,38 +17,39 @@ def typefile(request):
     return render(request, 'nutrition/typefile.html')
 
 def results(request):
-    food_list = ['apple', 'orange', 'grapefruit']
+    food_list = Food.objects.all()
+
     food_nutrition = {}
     
     for food in food_list:
 
         #get most likely food from search
-        x_app_id = '31acb5b6'
-        x_app_key = 'b7af62c2e4b293cf1ce74efb6b283935'
-        x_remote_user_id = '0'
+        #x_app_id = '31acb5b6'
+        #x_app_key = 'b7af62c2e4b293cf1ce74efb6b283935'
+        #x_remote_user_id = '0'
 
-        food_response = requests.get('https://trackapi.nutritionix.com/v2/search/instant?query=%s' % food, 
-            headers={"x-app-id":x_app_id, "x-app-key": x_app_key})
-        json_data_food = json.loads(food_response.text)
+        #food_response = requests.get('https://trackapi.nutritionix.com/v2/search/instant?query=%s' % food,
+        #    headers={"x-app-id":x_app_id, "x-app-key": x_app_key})
+        #json_data_food = json.loads(food_response.text)
 
-        food_name = json_data_food["common"][0]["food_name"].capitalize()
+        #food_name = json_data_food["common"][0]["food_name"].capitalize()
         #print(food_name)
 
         #get nutritional info of food
-        nutrition_response = requests.post('https://trackapi.nutritionix.com/v2/natural/nutrients', {"query": food_name}, 
-            headers={"x-app-id":x_app_id, "x-app-key": x_app_key})
+        #nutrition_response = requests.post('https://trackapi.nutritionix.com/v2/natural/nutrients', {"query": food_name},
+        #    headers={"x-app-id":x_app_id, "x-app-key": x_app_key})
 
-        json_data_nutrition = json.loads(nutrition_response.text)
+        #json_data_nutrition = json.loads(nutrition_response.text)
 
-        calories = json_data_nutrition['foods'][0]['nf_calories']
-        carbs = json_data_nutrition['foods'][0]['nf_total_carbohydrate']
-        protein = json_data_nutrition['foods'][0]['nf_protein']
-        fat = json_data_nutrition['foods'][0]['nf_total_fat']
-        sugar = json_data_nutrition['foods'][0]['nf_sugars']
+        #calories = json_data_nutrition['foods'][0]['nf_calories']
+        #carbs = json_data_nutrition['foods'][0]['nf_total_carbohydrate']
+        #protein = json_data_nutrition['foods'][0]['nf_protein']
+        #fat = json_data_nutrition['foods'][0]['nf_total_fat']
+        #sugar = json_data_nutrition['foods'][0]['nf_sugars']
 
-        food_nutrition[food_name] = {'name': food_name, 'calories':calories, 
-            'carbs': carbs, 'protein': protein, 'fat': fat, 'sugar': sugar}
-        print(food_nutrition[food_name])
+        food_nutrition[food.name] = {'name': food.name, 'calories':food.calories,
+            'carbs': food.carbs, 'protein': food.protein, 'fat': food.fat, 'sugar': food.sugar}
+        print(food_nutrition[food.name])
     
     return render(request, 'nutrition/results.html', 
         {'food_nutrition': food_nutrition})
@@ -80,6 +82,7 @@ def successful_google(request):
     for text in texts[1:]:
         print('\n"{}"'.format(text.description))
         answers.append("{}".format(text.description))
+
 
     return render(request, 'nutrition/successful_google.html', {'list': list, "answers": answers})
 
