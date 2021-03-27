ec5d4518039a6812c26ee584ddbd4ad3f373f5f2
KhalHajji <was5ev@virginia.edu>
2021-03-27 15:10:26 -0400

    successone

--- nutrition/urls.py
+++ nutrition/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('success/', views.success, name='success'),
+    path('typefile/', views.typefile, name='typefile')
 ]
--- nutrition/views.py
+++ nutrition/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'nutrition/home.html')
+
+def success(request):
+    return render(request, 'nutrition/success.html')
+
+def typefile(request):
+    return render(request, 'nutrition/typefile.html')
