f9660e0d63c2afe08ba7c74c7afe27dc36e607e7
KhalHajji <was5ev@virginia.edu>
2021-03-27 23:53:44 -0400

    hello

--- nutrition/views.py
+++ nutrition/views.py
@@ -13,6 +13,51 @@ def typefile(request):
     return render(request, 'nutrition/typefile.html')
 
 def success(request):
+    # food_list = Food.objects.all()
+    #
+    # food_nutrition = {}
+    # print("it workeds")
+    # for food in food_list:
+    #     # get most likely food from search
+    #     x_app_id = '57c64f2a'
+    #     x_app_key = '089056dd34e16a5d99331d52e729498c'
+    #     x_remote_user_id = '0'
+    #
+    #     food_response = requests.get('https://trackapi.nutritionix.com/v2/search/instant?query=%s' % "fruit",
+    #                                  headers={"x-app-id": x_app_id, "x-app-key": x_app_key})
+    #     json_data_food = json.loads(food_response.text)
+    #
+    #     food_data = {}
+    #
+    #     for food in json_data_food["common"]:
+    #         # get nutritional info of food
+    #         nutrition_response = requests.post('https://trackapi.nutritionix.com/v2/natural/nutrients',
+    #                                            {"query": food["food_name"]},
+    #                                            headers={"x-app-id": x_app_id, "x-app-key": x_app_key})
+    #
+    #         json_data_nutrition = json.loads(nutrition_response.text)
+    #
+    #         print(json_data_nutrition)
+    #
+    #         calories = json_data_nutrition['foods'][0]['nf_calories']
+    #         carbs = json_data_nutrition['foods'][0]['nf_total_carbohydrate']
+    #         protein = json_data_nutrition['foods'][0]['nf_protein']
+    #         fat = json_data_nutrition['foods'][0]['nf_total_fat']
+    #         sugar = json_data_nutrition['foods'][0]['nf_sugars']
+    #
+    #         food_data[food["food_name"]] = {"calories" : calories, "carbs" : carbs, "protein" : protein, "fat" : fat, "sugar" : sugar}
+    #
+    #
+    #
+    #     # food_name = json_data_food["common"][0]["food_name"].capitalize()
+    #     #print(json_data_food)
+    #
+    #
+    #     #
+    #     # food_nutrition[food.name] = {'name': food.name, 'calories': food.calories,
+    #     #                              'carbs': food.carbs, 'protein': food.protein, 'fat': food.fat, 'sugar': food.sugar}
+    #     # print(food_nutrition[food.name])
+
     return render(request, 'nutrition/success.html')
   
 def results(request):
@@ -88,42 +133,7 @@ def successful_google(request):
     return render(request, 'nutrition/successful_google.html', {'list': list, "answers": answers})
 
 
-# def find_tips(request):
-#     food_list = Food.objects.all()
-#
-#     food_nutrition = {}
-#
-#     for food in food_list:
-#         get most likely food from search
-#         x_app_id = '31acb5b6'
-#         x_app_key = 'b7af62c2e4b293cf1ce74efb6b283935'
-#         x_remote_user_id = '0'
-#
-#         food_response = requests.get('https://trackapi.nutritionix.com/v2/search/instant?query=%s' % food,
-#            headers={"x-app-id":x_app_id, "x-app-key": x_app_key})
-#         json_data_food = json.loads(food_response.text)
-#
-#         food_name = json_data_food["common"][0]["food_name"].capitalize()
-#         print(food_name)
-#
-#         get nutritional info of food
-#         nutrition_response = requests.post('https://trackapi.nutritionix.com/v2/natural/nutrients', {"query": food_name},
-#            headers={"x-app-id":x_app_id, "x-app-key": x_app_key})
-#
-#         json_data_nutrition = json.loads(nutrition_response.text)
-#
-#         calories = json_data_nutrition['foods'][0]['nf_calories']
-#         carbs = json_data_nutrition['foods'][0]['nf_total_carbohydrate']
-#         protein = json_data_nutrition['foods'][0]['nf_protein']
-#         fat = json_data_nutrition['foods'][0]['nf_total_fat']
-#         sugar = json_data_nutrition['foods'][0]['nf_sugars']
-#
-#         food_nutrition[food.name] = {'name': food.name, 'calories': food.calories,
-#                                      'carbs': food.carbs, 'protein': food.protein, 'fat': food.fat, 'sugar': food.sugar}
-#         print(food_nutrition[food.name])
-#
-#     return render(request, 'nutrition/success.html',
-#                   {'food_nutrition': food_nutrition})
+
 
 
 
